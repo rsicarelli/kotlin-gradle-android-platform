00588166695b967f37571c0d001012d3cba1c045
Rodrigo Sicarelli <rodrigo.sicarelli@gmail.com>
2023-09-21 20:40:38 +0200

    update compose

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -33,7 +33,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = libs.versions.composeKotlinCompilerExtension.get()
     }
     packaging {
         resources {
