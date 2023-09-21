008a0427de28bacba37d0265245a7439c801fccf
Rodrigo Sicarelli <rodrigo.sicarelli@gmail.com>
2023-09-21 19:55:30 +0200

    changed buildlogic to different folder

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -5,6 +5,6 @@ plugins {
 gradlePlugin {
     val greeting by plugins.creating {
         id = "build.logic.greeting"
-        implementationClass = "build.logic.BuildLogicPlugin"
+        implementationClass = "com.rsicarelli.buildlogic.BuildLogicPlugin"
     }
 }
