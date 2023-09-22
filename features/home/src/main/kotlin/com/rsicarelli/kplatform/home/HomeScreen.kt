fdf5f1a4ad80a86bb4131104932df2cb08abf7d1
Rodrigo Sicarelli <rodrigo.sicarelli@gmail.com>
2023-09-22 13:43:43 +0200

    use experimental material feature

--- features/home/src/main/kotlin/com/rsicarelli/kplatform/home/HomeScreen.kt
+++ features/home/src/main/kotlin/com/rsicarelli/kplatform/home/HomeScreen.kt
@@ -1,18 +1,35 @@
 package com.rsicarelli.kplatform.home
 
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.rsicarelli.kplatform.features.details.DetailsScreen
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen() {
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background
     ) {
-        DetailsScreen()
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .padding(all = 16.dp),
+            onClick = { },
+            content = {
+                DetailsScreen()
+            }
+        )
+
     }
 }
